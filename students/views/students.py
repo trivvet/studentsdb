8228555a4458ad331bed2d83fe0ea2822f5d0b6e
Trivvet <trivvet@gmail.com>
2015-06-23 20:56:15 +0300

    version 0.2.0 finish glava 6

--- students/views/students.py
+++ students/views/students.py
@@ -46,32 +46,3 @@ def students_edit(request, sid):
 def students_delete(request, sid):
 	return HttpResponse('<h1>Delete Student %s</h1>' % sid)
 	
-# Views for Groups
-
-def groups_list(request):
-	groups = (
-		{'id': 1,
-		 'name': u'1Б-04',
-		 'leader': {'id': 4, 'name': u'Бацура Олександр'}},
-		{'id': 2,
-		 'name': u'2Б-04',
-		 'leader': {'id': 5, 'name': u'Козаченко Богдан'}},
-		{'id': 3,
-		 'name': u'БМ-04',
-		 'leader': {'id': 2, 'name': u'Люціус Мелфой'}},
-		 )
-	return render(request, 'students/groups_list.html', {'groups': groups})
-
-def groups_add(request):
-	return HttpResponse('<h1>Group Add Form</h1>')
-	
-def groups_edit(request, gid):
-	return HttpResponse('<h1>Edit Group %s</h1>' % gid)
-	
-def groups_delete(request, gid):
-	return HttpResponse('<h1>Delete Group %s</h1>' % gid)
-	
-# Views for Journal
-
-def journal_list(request):
-	return HttpResponse('<h1>Journal</h1>')
